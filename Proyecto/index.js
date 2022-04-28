fea65918db20b452f9b17e6ee74dc9ac97f00e7a
DesiAPG <desireapgames@gmail.com>
2022-04-28 08:01:08 -0500

    routes

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -1,12 +1,16 @@
 const path = require("path");
+
+// Módulos Externos
 const express = require("express");
 const port = 4000;
 const app = express();
 
+// Ruta Principal ('/')
 app.get("/", (req, res) => {
-  return res.sendFile();
+  return res.sendFile(path.join(__dirname, "static", "index.html"));
 });
 
-app.listen(port, (req, res) => {
+// Puerto de escucha
+app.listen(port, () => {
   console.log("Listening on port " + port);
 });
