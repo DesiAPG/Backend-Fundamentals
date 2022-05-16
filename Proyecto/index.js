d9d2a54a03b58c20ca58e418720b2b1c3669fdfa
DesiAPG <desireapgames@gmail.com>
2022-05-15 21:32:25 -0500

    hbs

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -5,14 +5,27 @@ const express = require("express");
 const port = 4000;
 const users = require("./routes/users");
 const app = express();
+const exphbs = require("express-handlebars");
 
+//Handlebars
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views") + "layouts"),
+    partialsDir: path.join(app.get("views") + "partials"),
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "handlebars");
 
 // Rutas importadas
 users(app);
 
 // Ruta Principal ('/')
 app.get("/", (req, res) => {
-  
+  return res.render("index");
 });
 
 // Puerto de escucha
