b9b4ce66a4e199b3070466d9c05a0bee9ceaac9c
DesiAPG <desireapgames@gmail.com>
2022-05-26 08:17:51 -0500

    Added env variables

--- project-music/config/index.js
+++ project-music/config/index.js
@@ -1,7 +1,14 @@
-require("dotenv").config;
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const config = {
   port: process.env.PORT,
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+  dbUserName: process.env.DB_USERNAME,
+  dbName: process.env.DB_NAME,
+  dbPassword: process.env.DB_PASSWORD,
 };
 
 module.exports = config;
