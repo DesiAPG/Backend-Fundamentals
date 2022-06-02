3e626d6f29191ca851a0333a4f06a9c7795f7082
DesiAPG <desireapgames@gmail.com>
2022-06-01 21:56:01 -0500

    refactoring code

--- project-music/routes/auth.js
+++ project-music/routes/auth.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const { getLoginForm } = require("../controllers/auth");
+const AuthController = require("../controllers/auth");
 
 const router = express.Router();
 
 router.get("/login", (req, res) => {
-  return res.render("login", {
-    username: "Desire",
-    lista: ["María", "Miguel", "Emilio"],
-    id: 123,
-  });
+  return res.render("login", getLoginForm);
 });
+
+router.post("/login", AuthController.login);
 
 module.exports = router;
