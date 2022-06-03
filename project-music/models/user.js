23fb6635038fb1184388c395cfefff04e3ff3772
DesiAPG <desireapgames@gmail.com>
2022-06-02 21:23:40 -0500

    user model

---
+++ project-music/models/user.js
@@ -0,0 +1,20 @@
+const { query } = require("../libs/database");
+
+class User {
+  static async getByEmail(email) {
+    try {
+      const user = await query("SELECT * FROM users WHERE email=?", [email]);
+      return {
+        success: true,
+        user: user[0],
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: "An error ocurred",
+      };
+    }
+  }
+}
+
+module.exports = User;
